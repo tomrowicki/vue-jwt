a0755c549ce2610ef2574d13bba5dd002623a17b
Tomasz Mrowicki <tomrowicki@gmail.com>
2019-06-08 22:07:54 +0200

    Lesson 5 finished

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/recommended', 'standard'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 4,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'space-before-function-paren': ['error', 'never']
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
